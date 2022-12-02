61c3bfccd3a6426777d895f5ddf99d2c7e3213c3
Kyler Johnson <kylerjohnsondev@gmail.com>
2022-12-02 01:41:29 -0500

    add action groups

--- src/app/pages/customers/customers-feature-state/customers.actions.ts
+++ src/app/pages/customers/customers-feature-state/customers.actions.ts
@@ -1,28 +1,19 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { createAction, props } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 import { CustomerPayload } from 'src/app/shared/models';
 
-const loadCustomers = createAction('[Customers Effects] load customers');
-const loadCustomersSuccess = createAction(
-  '[Customers Effects] load customers success',
-  props<{ payload: CustomerPayload }>()
-);
-const loadCustomersFailure = createAction(
-  '[Customers Effects] load customers error',
-  props<{ error: HttpErrorResponse }>()
-);
+export const customersEffectsActions = createActionGroup({
+  source: 'Customers Effects',
+  events: {
+    'load customers': emptyProps(),
+    'load customers success': props<{ payload: CustomerPayload }>(),
+    'load customers Failure': props<{ error: HttpErrorResponse }>(),
+  },
+});
 
-export const customersEffectsActions = {
-  loadCustomers,
-  loadCustomersSuccess,
-  loadCustomersFailure,
-};
-
-const paginateCustomers = createAction(
-  '[Customers Page] paginate customers table',
-  props<{ pageIndex: number; pageSize: number }>()
-);
-
-export const customersPageActions = {
-  paginateCustomers,
-};
+export const customersPageActions = createActionGroup({
+  source: 'Customers Page',
+  events: {
+    'paginate customers': props<{ pageIndex: number; pageSize: number }>(),
+  },
+});
