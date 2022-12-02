f4d312166b0633bc865c52a9be49afd61cafc9de
Kyler Johnson <kylerjohnsondev@gmail.com>
2022-12-02 01:49:32 -0500

    add feature creator

--- src/app/pages/customers/customers-feature-state/customers.reducer.ts
+++ src/app/pages/customers/customers-feature-state/customers.reducer.ts
@@ -1,12 +1,10 @@
 import { createEntityAdapter, EntityState } from '@ngrx/entity';
-import { createReducer, on } from '@ngrx/store';
+import { createFeature, createReducer, on } from '@ngrx/store';
 import { Customer } from 'src/app/shared/models';
 import {
   customersEffectsActions,
   customersPageActions,
 } from './customers.actions';
-
-export const CUSTOMERS_FEATURE_KEY = 'Customers';
 
 export interface CustomersState extends EntityState<Customer> {
   loading: boolean;
@@ -28,40 +26,43 @@ const initialState: CustomersState = customersAdapter.getInitialState({
   totalCount: 0,
 });
 
-export const customersReducer = createReducer(
-  initialState,
+export const customersFeature = createFeature({
+  name: 'Customers',
+  reducer: createReducer(
+    initialState,
 
-  on(customersEffectsActions.loadCustomers, (state) => ({
-    ...state,
-    loading: true,
-  })),
-
-  on(customersEffectsActions.loadCustomersSuccess, (state, { payload }) => {
-    return customersAdapter.setAll(payload.customer, {
+    on(customersEffectsActions.loadCustomers, (state) => ({
       ...state,
-      totalCount: payload.customer_aggregate.aggregate.count,
-      loading: false,
-      error: null,
-    });
-  }),
+      loading: true,
+    })),
 
-  on(customersEffectsActions.loadCustomersFailure, (state, { error }) => ({
-    ...state,
-    error: error.message,
-    loading: false,
-  })),
-
-  on(
-    customersPageActions.paginateCustomers,
-    (state, { pageIndex, pageSize }) => {
-      const offset = state.limit * pageIndex;
-      return {
+    on(customersEffectsActions.loadCustomersSuccess, (state, { payload }) => {
+      return customersAdapter.setAll(payload.customer, {
         ...state,
-        loading: true,
-        pageIndex,
-        offset,
-        limit: pageSize,
-      };
-    }
-  )
-);
+        totalCount: payload.customer_aggregate.aggregate.count,
+        loading: false,
+        error: null,
+      });
+    }),
+
+    on(customersEffectsActions.loadCustomersFailure, (state, { error }) => ({
+      ...state,
+      error: error.message,
+      loading: false,
+    })),
+
+    on(
+      customersPageActions.paginateCustomers,
+      (state, { pageIndex, pageSize }) => {
+        const offset = state.limit * pageIndex;
+        return {
+          ...state,
+          loading: true,
+          pageIndex,
+          offset,
+          limit: pageSize,
+        };
+      }
+    )
+  ),
+});
